1b498233b7ffdd552a1c01cfd790afa45960b027
ArtieFuzzz <hey@astolfo.cafe>
2025-06-08 22:59:50 +1000

    Include rel in social links

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-import "iconify-icon"; // Create some animation thingymabob
+  import "iconify-icon"; // Create some animation thingymabob
+  // TODO: Rename this component to SocialLink.svelte
 
-const props: { link: string; name: string; icon: string } = $props();
+  const props: { link: string; name: string; icon: string } = $props();
 </script>
 
 <a
   href={props.link}
+  rel="me"
   class="inline-flex items-center text-base font-body font-normal text-white gap-3"
 >
   <iconify-icon icon={props.icon} width="24" height="24"></iconify-icon>
